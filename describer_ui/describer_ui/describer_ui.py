f8f03a6b433da98c972e0d4f85a75ee1765184ee
fatmaann <sol1222183@gmail.com>
2024-10-02 12:34:24 +0300

    feature photo_page init

--- describer_ui/describer_ui/describer_ui.py
+++ describer_ui/describer_ui/describer_ui.py
@@ -14,11 +14,13 @@ def index() -> rx.Component:
         rx.center(
             rx.hstack(
                 rx.button(rx.icon(tag="image-up"),
-                          "Photo", on_click=lambda: rx.window_alert("Photo"), width="500px", height="400px",
-                          font_size="35px", background_color="#c7522a", ),
+                          "Photo", on_click=lambda: rx.redirect("/photo"),
+                          width="500px", height="400px",
+                          font_size="35px", background_color="#b25938", ),
                 rx.button(rx.icon(tag="clipboard-pen"),
-                          "Text", on_click=lambda: rx.window_alert("Text"), width="500px", height="400px",
-                          font_size="35px", background_color="#e5c185", ),
+                          "Text", on_click=lambda: rx.redirect("/text"),
+                          width="500px", height="400px",
+                          font_size="35px", background_color="#c2b280", ),
                 spacing="5",
             ),
             height="70vh",
@@ -26,5 +28,57 @@ def index() -> rx.Component:
     )
 
 
+def photo_page() -> rx.Component:
+    return rx.container(
+        rx.center(
+            rx.vstack(
+                rx.center(
+                    rx.heading("Photo Page", size="8", color="white"),
+                    height="10vh"
+                ),
+                rx.upload(
+                    rx.vstack(
+                        rx.button(
+                            "Select File",
+                            size="4",
+                            color="white",
+                            bg="#111113",
+                            width="100%",
+                            height="100%",
+                            color_scheme='gray'
+                        ),
+                    ),
+                    id="upload1",
+                    border=f"1px solid white",
+                    border_radius="10px",
+                    width="500px",
+                    height="190px",
+                    align_items="center",
+                    justify_content="center",
+
+                ),
+                rx.input(
+                    placeholder="Write a description here...",
+                    width="500px",
+                    color_scheme='gray',
+                    variant="soft",
+                ),
+            ),
+        ),
+    )
+
+
+def text_page() -> rx.Component:
+    return rx.container(
+        rx.center(
+            rx.heading("Text Page", size="8"),
+            height="20vh"
+        ),
+        height="100%",
+    )
+
+
 app = rx.App()
-app.add_page(index)
+app.add_page(index, route="/")
+app.add_page(photo_page, route="/photo")
+app.add_page(text_page, route="/text")
