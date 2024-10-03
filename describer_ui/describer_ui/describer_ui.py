30d53725ebd6518af6bf69713e7cb8c007a666f4
fatmaann <sol1222183@gmail.com>
2024-10-04 02:30:24 +0300

    chore file uploading

--- describer_ui/describer_ui/describer_ui.py
+++ describer_ui/describer_ui/describer_ui.py
@@ -1,10 +1,22 @@
+import base64
 import reflex as rx
-from rxconfig import config
 
 
 class State(rx.State):
-    """The app state."""
-    ...
+    img: list[str] = []
+
+    async def handle_upload(self, files: list[rx.UploadFile]):
+        for file in files:
+            upload_data = await file.read()
+            base64_str = base64.b64encode(upload_data).decode('utf-8')
+            self.img.append(base64_str)
+
+    async def get_description(self):
+        if len(self.img) == 0:
+            print('get_description empty')
+        else:
+            for file in self.img:
+                print('get_description', len(file))
 
 
 def index() -> rx.Component:
@@ -13,14 +25,10 @@ def index() -> rx.Component:
         rx.center(rx.heading("Welcome to LLM Product Describer!", size="8"), height="20vh"),
         rx.center(
             rx.hstack(
-                rx.button(rx.icon(tag="image-up"),
-                          "Photo", on_click=lambda: rx.redirect("/photo"),
-                          width="500px", height="400px",
-                          font_size="35px", background_color="#b25938", ),
-                rx.button(rx.icon(tag="clipboard-pen"),
-                          "Text", on_click=lambda: rx.redirect("/text"),
-                          width="500px", height="400px",
-                          font_size="35px", background_color="#c2b280", ),
+                rx.button(rx.icon(tag="image-up"), "Photo", on_click=lambda: rx.redirect("/photo"),
+                          width="500px", height="400px", font_size="35px", background_color="#b25938"),
+                rx.button(rx.icon(tag="clipboard-pen"), "Text", on_click=lambda: rx.redirect("/text"),
+                          width="500px", height="400px", font_size="35px", background_color="#c2b280", ),
                 spacing="5",
             ),
             height="70vh",
@@ -33,7 +41,7 @@ def photo_page() -> rx.Component:
         rx.center(
             rx.vstack(
                 rx.center(
-                    rx.heading("Photo Page", size="8", color="white"),
+                    rx.heading("Photo Page", size="9", color="white"),
                     height="10vh"
                 ),
                 rx.upload(
@@ -49,20 +57,25 @@ def photo_page() -> rx.Component:
                         ),
                     ),
                     id="upload1",
+                    max_files=1,
                     border=f"1px solid white",
                     border_radius="10px",
                     width="500px",
                     height="190px",
                     align_items="center",
                     justify_content="center",
-
+                    on_drop=State.handle_upload(rx.upload_files(upload_id="upload1")),
                 ),
                 rx.input(
                     placeholder="Write a description here...",
                     width="500px",
+                    height="40px",
                     color_scheme='gray',
                     variant="soft",
+                    font_size='12pt'
                 ),
+                rx.button("Generate", width="200px", height="40px", color_scheme='gray', font_size='12pt',
+                          on_click=State.get_description),
             ),
         ),
     )
